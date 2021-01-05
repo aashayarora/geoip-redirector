01512edc2e8e6e0ac2244cd28d978a371703460f
aaarora <aaarora@ucsd.edu>
2021-01-04 17:27:19 -0800

    update

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM opensciencegrid/software-base:fresh
 LABEL maintainer OSG Software <help@opensciencegrid.org>
 
 RUN yum -y install vim emacs && \
-    yum -y install mod_wsgi
+    yum -y install mod_wsgi && \
+    yum -y install python-pip
 
-ADD geoip.conf /etc/httpd/conf.d/geoip.conf
+RUN pip install flask
+
 ADD supervisord.conf /etc/supervisord.d/
 ADD image-config.d/* /etc/osg/image-config.d/
--- geoip.py
+++ geoip.py
@@ -1,9 +1,9 @@
-def application(environ, start_response):
-    status = '200 OK'
-    output = b'Hello World!'
+from flask import Flask
+app = Flask(__name__)
 
-    response_headers = [('Content-type', 'text/plain'),
-                        ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-    return [output]
+if __name__ == '__main__':
+    app.run()
